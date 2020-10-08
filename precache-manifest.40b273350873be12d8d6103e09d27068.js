342c968efc85b101e1a677e16eaf68edf29e9ebc
whmaid <whmaid@benchmarkconsulting.com>
2020-10-08 17:21:01 -0400

    Updates

---
+++ precache-manifest.40b273350873be12d8d6103e09d27068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee37e4da5bd4ac123968acec62d5f53",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c909c68da31873a2d03",
+    "url": "/todolist/static/js/main.67d2dae1.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
